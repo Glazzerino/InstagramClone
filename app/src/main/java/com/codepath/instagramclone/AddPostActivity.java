a7c14d52249e0ab1c17932c5af36e7969825d3f0
Glazzerino <fztmon@gmail.com>
2021-07-07 18:27:10 -0500

    image capture

--- app/src/main/java/com/codepath/instagramclone/AddPostActivity.java
+++ app/src/main/java/com/codepath/instagramclone/AddPostActivity.java
@@ -1,5 +1,7 @@
 package com.codepath.instagramclone;
 
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.TextureView;
@@ -12,6 +14,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -24,7 +29,10 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.io.File;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class AddPostActivity extends AppCompatActivity {
 
@@ -34,6 +42,10 @@ public class AddPostActivity extends AppCompatActivity {
     Button btnPost;
     Button btnShutter;
     PreviewView pvCameraPreview;
+    //Used to set and manage photo taking
+    ImageCapture imageCapture;
+    CameraSelector cameraSelector;
+    String photoUri;
     public static final String TAG = "AddPostActivity";
 
     @Override
@@ -45,18 +57,55 @@ public class AddPostActivity extends AppCompatActivity {
         btnPost = findViewById(R.id.btnPost);
         btnShutter = findViewById(R.id.btnShutter);
         pvCameraPreview = findViewById(R.id.pvCameraPreview);
+        cameraSelector = new CameraSelector.Builder()
+                //TODO: Add camera selection for selfies
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
 
-        //Camera setup. Listener
+        photoUri = "";
+        //Camera setup + Listener
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        //Offload process to a new thread managed by the ListenableFuture class
+        imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(this.getDisplay().getRotation())
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+                .build();
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                 bindToPreview(cameraProvider);
             } catch (ExecutionException | InterruptedException e) {
-                // No errors need to be handled for this Future.
-                // This should never be reached.
+                //this isnt supposed to ever happen
+                Log.e(TAG, "Error during CameraX bind to preview: " + e.toString());
             }
         }, ContextCompat.getMainExecutor(this));
+
+
+
+        btnShutter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Declare filename as current time in seconds and .jpg in current directory
+                    photoUri = String.valueOf(new Date().getTime() / 1000) + ".jpg";
+                    ImageCapture.OutputFileOptions outputFileOptions =
+                            new ImageCapture.OutputFileOptions.Builder(new File(getFilesDir()
+                                    ,photoUri)).build();
+
+                    imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(AddPostActivity.this),
+                            new ImageCapture.OnImageSavedCallback() {
+                                @Override
+                                public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                                    // insert your code here.
+                                    Log.d(TAG, "image saved: " + getFilesDir()+photoUri);
+                                }
+                                @Override
+                                public void onError(ImageCaptureException error) {
+                                    // insert your code here.
+                                }
+                            }
+                    );
+                }
+            });
 
         btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,13 +125,15 @@ public class AddPostActivity extends AppCompatActivity {
     void bindToPreview(@NonNull ProcessCameraProvider cameraProvider) {
 
         CameraSelector cameraSelector = new CameraSelector.Builder()
+                //TODO: Add camera selection for selfies
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
         Preview preview = new Preview.Builder().build();
         preview.setSurfaceProvider(pvCameraPreview.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, imageCapture, preview);
     }
+
     private void savePost(String description, ParseUser user) {
         // Create new post object
         Post post = new Post();
